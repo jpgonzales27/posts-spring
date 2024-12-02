822efac62f993e3e404e579fb59f2abbf533fe2e
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-12-02 10:31:10 -0400

    Creando controller advice para manejo de errores

---
+++ src/main/java/com/jp/posts/controllers/ErrorHandlerController.java
@@ -0,0 +1,25 @@
+package com.jp.posts.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ErrorHandlerController {
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    private ResponseEntity<Map<String, Object>>
+    IllegalArgumentHandler(IllegalArgumentException ex) {
+        final var response = new HashMap<String, Object>();
+
+        response.put("code", HttpStatus.BAD_REQUEST.value());
+        response.put("status", HttpStatus.BAD_REQUEST.getReasonPhrase());
+        response.put("message", ex.getMessage());
+
+        return ResponseEntity.badRequest().body(response);
+    }
+}
