94c121ac342dbb44e35b92c83ed34ce1ca7a4831
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-12-01 21:51:07 -0400

    creando query methods y su version con jpql

--- src/main/java/com/jp/posts/repositories/PageRepository.java
+++ src/main/java/com/jp/posts/repositories/PageRepository.java
@@ -2,6 +2,21 @@ package com.jp.posts.repositories;
 
 import com.jp.posts.entities.PageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface PageRepository extends JpaRepository<PageEntity,Long> {
+
+    /**
+     *  Query Methods
+     */
+    //Select * from page where title = ?1
+    Optional<PageEntity> findByTitle(String title);
+
+    /**
+     *  JPQL
+     */
+    @Query("from PageEntity where title = :title")
+    Optional<PageEntity> findByTitle2(String title);
 }
