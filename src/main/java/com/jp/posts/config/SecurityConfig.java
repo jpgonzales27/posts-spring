384236c0326fc3cc9d06f39c0f06841933599722
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-12-02 20:22:52 -0400

    Agregando security started y iniciando basic security config

---
+++ src/main/java/com/jp/posts/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.jp.posts.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
--- src/main/java/com/jp/posts/repositories/UserRepository.java
+++ src/main/java/com/jp/posts/repositories/UserRepository.java
@@ -3,5 +3,10 @@ package com.jp.posts.repositories;
 import com.jp.posts.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
+
+    //Select * FROM users WHERE mail = :mail
+    Optional<UserEntity> findByMail(String mail);
 }
